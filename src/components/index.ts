a95b26d25be370a1c6298e6363ac8c976336cdaa
Darth Vve <osheviremajoroh@gmail.com>
2023-05-29 00:25:41 +0100

    Version 1.3

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+import Loader from './loader/index';
 export { default as Navbar } from './navbar';
 export { default as MobileNav } from './navbar/mobile-nav';
 export { default as Footer } from './footer';
@@ -7,3 +8,4 @@ export { default as Partners } from './partners';
 export { default as WhatWeDo } from './what';
 export { default as HowWeMakeIt } from './how';
 export { default as ContactAd } from './contactAd';
+export { default as Loader } from './loader';
